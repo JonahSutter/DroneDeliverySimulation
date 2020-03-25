6352ae8c017b6763ebad220b1ef75cc71b35c3b6
LISLEES17 <LISLEES17@5CG7310CTN.GCC.edu>
2020-03-25 14:38:28 -0400

    main page and opening screen started

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,182 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
-public class Main {
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+public class Main extends Application {
 
 	
 	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+			primaryStage.setTitle("Starting Screen");
+			
+			Label label = new Label("Dromedary Drones");
+			
+			label.setFont(new Font("Arial", 40));
+			label.setLayoutX(80);
+
+	        Button button1 = new Button("Open");
+	        Button button2 = new Button("Exit");
+	        
+	        button1.setPrefHeight(40);
+	        button1.setPrefWidth(140);
+	        button2.setPrefHeight(40);
+	        button2.setPrefWidth(140);
+	        
+	   
+	        
+	        Pane root = new Pane();
+	        button1.setLayoutX(180);
+	        button1.setLayoutY(200);
+	        button2.setLayoutX(180);
+	        button2.setLayoutY(250);
+
+	     
+	        root.getChildren().add(button1);
+	        root.getChildren().add(button2);
+	        root.getChildren().add(label);
+	        
+	        button1.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+	            @Override public void handle(ActionEvent e) {
+	                mainPage(primaryStage);
+	            }
+	        });
+	        
+	        
+	        button2.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+	            @Override public void handle(ActionEvent e) {
+	                System.exit(1);
+	            }
+	        });
+			
+			Scene scene = new Scene(root,500,500);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		
+			new AnimationTimer() {
+				@Override
+				public void handle(long now) {
+					
+					
+				}
+			}.start();
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
-	public static void runGUI() {
+	public void mainPage(Stage primaryStage) {
+		try {
+			primaryStage.setTitle("Main Page");
+			
+			Label label = new Label("Dromedary Drones");
+			
+			label.setFont(new Font("Arial", 40));
+			label.setLayoutX(80);
+
+	        Button button1 = new Button("Start Simulation");
+	        Button button2 = new Button("Edit Meals");
+	        Button button3 = new Button("Edit Probabilities");
+	        Button button4 = new Button("Exit");
+	        
+	   
+	        
+	        Pane root = new Pane();
+	        button1.setLayoutX(180);
+	        button1.setLayoutY(200);
+	        button2.setLayoutX(180);
+	        button2.setLayoutY(250);
+	        button3.setLayoutX(180);
+	        button3.setLayoutY(300);
+	        button4.setLayoutX(180);
+	        button4.setLayoutY(350);
+	        
+	        button1.setPrefHeight(40);
+	        button1.setPrefWidth(140);
+	        button2.setPrefHeight(40);
+	        button2.setPrefWidth(140);
+	        button3.setPrefHeight(40);
+	        button3.setPrefWidth(140);
+	        button4.setPrefHeight(40);
+	        button4.setPrefWidth(140);
+	     
+	        root.getChildren().add(button1);
+	        root.getChildren().add(button2);
+	        root.getChildren().add(button3);
+	        root.getChildren().add(button4);
+	        root.getChildren().add(label);
+	        
+	        button1.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+	            @Override public void handle(ActionEvent e) {
+	                simmulationPage();
+	            }
+	        });
+	        
+	        button2.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+	            @Override public void handle(ActionEvent e) {
+	                meals();
+	            }
+	        });
+	        
+	        button3.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+	            @Override public void handle(ActionEvent e) {
+	                probabilityPage();
+	            }
+	        });
+	        
+	        button4.setOnAction((EventHandler<ActionEvent>) new EventHandler<ActionEvent>() {
+	            @Override public void handle(ActionEvent e) {
+	                System.exit(1);
+	            }
+	        });
+			
+			Scene scene = new Scene(root,500,500);
+			
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
 		
+			new AnimationTimer() {
+				@Override
+				public void handle(long now) {
+					
+					
+				}
+			}.start();
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public static void simmulationPage() {
