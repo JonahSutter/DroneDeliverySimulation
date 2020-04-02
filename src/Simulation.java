1839041ad9dd17695521cddd961fbda7d086b53b
Bennett <BENNETTMC17@GCC.EDU>
2020-04-02 09:24:24 -0400

    Initial workings of knapsack

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,26 +1,35 @@
 
 public class Simulation {
-	
+
 	private void knapSack() {
 		//Pretend orderList is the list of orders
+		/*
 		int time = 0;
-		
+		int pointer = 0;
+
+		while (pointer < orderList.size()) {
+			if (orderList.get(pointer).getTime() <= time) {
+				//Add to current orders
+			}
+			time++;
+		}
+		*/
 	}
-	
+
 	private void FIFO() {
-		
+
 	}
-	
+
 	public void runSimulation() {
-		
+
 	}
-	
+
 	public void displayMethod() {
-		
+
 	}
-	
+
 	public void saveResults() {
-		
+
 	}
-	
+
 }
