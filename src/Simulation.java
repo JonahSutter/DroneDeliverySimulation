e810a821d3dcb82ee7dd23a96dff1d1ae97bb728
Bennett <BENNETTMC17@GCC.EDU>
2020-04-02 11:37:49 -0400

    More knapsack updates

--- src/Simulation.java
+++ src/Simulation.java
@@ -2,19 +2,24 @@
 public class Simulation {
 
 	private void knapSack() {
-		//Pretend orderList is the list of orders
+		//**Pretend orderList is the list of orders**
+
 		/*
-		int time = 0; //Every increment of time = 1 second
-		int pointer = 0; //Used to go through the generated orders list
+		int time = 0; 		//Every increment of time = 1 second
+		int pointer = 0; 	//Used to go through the generated orders list
+		double currWeight = 0; //The current weight loaded on the drone.
 
 		//List of current orders the Drone can see
 		ArrayList<Meal> currentOrders = new ArrayList<Meal>();
 
-		double droneSpeed = 29.3333 //Speed is in feet per second
-		int dropoffTime = 30  //Time is in seconds
-		int maxWeight = 192 //Weight in ounces
-		int turnAround = 180 //Time is in seconds
-		boolean wasSent = false;
+		//List of meals we skipped over when doing knapsack packing
+		ArrayList<Meal> skippedOrders = new ArrayList<Meal>();
+
+		double droneSpeed = 29.3333 	//Speed is in feet per second. (20mph)
+		double dropoffTime = 30  		//Time is in seconds (30 sec)
+		double maxWeight = 192 			//Weight in ounces (12 lbs)
+		double turnAround = 180 		//Time is in seconds (3 min)
+		boolean wasSent = false;		//Set to true if the drone has delivered
 
 		while (pointer < orderList.size()) {
 
@@ -24,10 +29,23 @@ public class Simulation {
 				pointer++;
 			}
 
+
 			//The drone will only leave if there is a current order
-			//(Except for the first 5 minutes - special case)
+				//(Except for the first 5 minutes - special case)
 			if (currentOrders.size() > 0 && time >= 300) {
+
 				//Pack the orders (Don't go overweight)
+				while (currentOrders.size() > 0 && currWeight < maxWeight) {
+					double maxWeight = 0;
+					int mealPos = 0;
+					for (int i = 0; i < currentOrders.size(); i++) {
+						double mealWeight = currentOrder.get(i).getWeight();
+						if (mealWeight > maxWeight) {
+							mealPos = i;
+							maxWeight = mealWeight;
+						}
+					}
+				}
 
 				//Calculate how long the drone will be gone
 				//   and increment the time by that value - 1
