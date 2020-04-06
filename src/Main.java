93ca9cbf78fb9ce4d2ca2bc7b5db69edf1edae36
Jonah <jonah@jonahsutter.com>
2020-04-06 16:15:03 -0400

    Added additional functionality to meals and add meals pages
    Meals page now displays current meals. Add meals page GUI has
    functionality and will actually add a meal

--- src/Main.java
+++ src/Main.java
@@ -333,7 +333,7 @@ public class Main extends Application {
 			label.setLayoutX(80);
 
 
-			Label mealLabel = new Label("Meal Name:");
+			Label mealLabel = new Label("Meals:");
 			mealLabel.setFont(new Font("Arial",20));
 			mealLabel.setLayoutX(250);
 			mealLabel.setLayoutY(72);
