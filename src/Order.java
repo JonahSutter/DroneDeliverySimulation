9112333c089c022f4b2b0017177eab940152a565
Bennett <BENNETTMC17@GCC.EDU>
2020-04-06 13:22:03 -0400

    Implement getters for Order and Orders. Orders List stored as an array

--- src/Order.java
+++ src/Order.java
@@ -1,14 +1,26 @@
 
 public class Order {
 	private int timeStamp;
-	private int[] location;
+	private double[] location;
 	private Meal meal;
-	
-	public Order(int timeStamp, int[] location, Meal meal) {
+
+	public Order(int timeStamp, double[] location, Meal meal) {
 		this.meal = meal;
 		this.timeStamp = timeStamp;
 		this.location[0] = location[0];
 		this.location[1] = location[1];
+	}
+
+	public int getTime() {
+		return timeStamp;
+	}
+
+	public double[] getLocation() {
+		return location;
+	}
+
+	public Meal getMeal() {
+		return meal;
 	}
 
 }
--- src/Orders.java
+++ src/Orders.java
@@ -9,101 +9,101 @@ public class Orders {
 	private int thirdHour;
 	private int fourthHour;
 	private ArrayList<Meal> meals;
-	private Queue<Order> orders;
+	private ArrayList<Order> orders;
 
-	
-	public Orders(int firstHour, int secondHour, int thirdHour, int fourthHour, ArrayList<Meal> meals, int[][] locations) {
+
+	public Orders(int firstHour, int secondHour, int thirdHour, int fourthHour, ArrayList<Meal> meals, double[][] locations) {
 		this.firstHour = firstHour;
 		this.secondHour = secondHour;
 		this.thirdHour = thirdHour;
 		this.fourthHour = fourthHour;
-		
+
 		ArrayList<Meal> percentages = new ArrayList<Meal>();
-		
+
 		for (Meal m : meals) {
 			this.meals.add(m);
-			
+
 			for (int i = 0; i < m.getPercentage(); i++) {
 				percentages.add(m);
 			}
 
 		}
-		
-		
+
+
 		int numSoFar = 0;
 		int min = 0;
-		
+
 		Random rand = new Random();
-		
-		
-		
+
+
+
 		while (numSoFar < firstHour) {
 
-			
+
 			if (rand.nextDouble() <= (firstHour - numSoFar)/(60 - min)){
 				int meal = rand.nextInt(100);
 				int location = rand.nextInt(locations.length);
-				
+
 				orders.add(new Order(min, locations[location], percentages.get(meal)));
-				
+
 			}
 
 			min++;
 		}
-		
+
 		min = 60;
-		
+
 		while (numSoFar < secondHour) {
 
 			if (rand.nextDouble() <= (secondHour - numSoFar)/(120 - min)){
 				int meal = rand.nextInt(100);
 				int location = rand.nextInt(locations.length);
-				
+
 				orders.add(new Order(min, locations[location], percentages.get(meal)));
-				
+
 			}
 
 			min++;
 		}
-		
-		
+
+
 		min = 120;
-		
+
 		while (numSoFar < thirdHour) {
 
 			if (rand.nextDouble() <= (thirdHour - numSoFar)/(180 - min)){
 				int meal = rand.nextInt(100);
 				int location = rand.nextInt(locations.length);
-				
+
 				orders.add(new Order(min, locations[location], percentages.get(meal)));
-				
+
 			}
 
 			min++;
 		}
-		
+
 		min = 180;
-		
+
 		while (numSoFar < fourthHour) {
 
 			if (rand.nextDouble() <= (fourthHour - numSoFar)/(240 - min)){
 				int meal = rand.nextInt(100);
 				int location = rand.nextInt(locations.length);
-				
+
 				orders.add(new Order(min, locations[location], percentages.get(meal)));
-				
+
 			}
 
 			min++;
 		}
-		
-		
+
+
 	}
-	
-	public Order getOrder() {
-		return orders.poll();
+
+	public ArrayList<Order> getOrders() {
+		return orders;
 	}
-	
+
 
 
 
