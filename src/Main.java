c60ff183d50399e0d4f82da0494be8573b96e807
Bennett <BENNETTMC17@GCC.EDU>
2020-04-29 15:58:53 -0400

    Undoing comment out changes

--- src/Main.java
+++ src/Main.java
@@ -75,7 +75,7 @@ public class Main extends Application {
 	private static Food f1 = new Food("1/4 lb Hamburger", 6);
 	private static Food f2 = new Food("Fries", 4);
 	private static Food f3 = new Food("12 oz Drink", 14);
-//	private static Image image = new Image(Main.class.getResourceAsStream("mapGroveCity.jpg"));
+	private static Image image = new Image(Main.class.getResourceAsStream("mapGroveCity.jpg"));
 
 
 	public static void main(String[] args) {
@@ -2544,7 +2544,7 @@ public class Main extends Application {
 
 		           	if (file != null) {
 		           		try {
-//			                image = new Image("file:" + file.getAbsolutePath());
+			                image = new Image("file:" + file.getAbsolutePath());
 		           		}
 		           		catch(Exception e1) {
 		           			e1.printStackTrace();
@@ -2676,7 +2676,7 @@ public class Main extends Application {
 
 		//creates the image
 		ImageView selectedImage = new ImageView();
-//		selectedImage.setImage(image);
+		selectedImage.setImage(image);
 
 
 		//the image will resize with the window
